1db64caa72e33610207590c56aab35fee2042030
jairoogp <jairoguemes05@iesamachado.org>
2024-12-12 14:28:29 +0100

    asdada

---
+++ php/php(3)/form.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Examen</title>
+</head>
+<body>
+    <form id="formulario" method="POST" action="form.php">
+        <input type="number" name="numero" id="numero" placeholder="Introduce un número positivo">
+        <input type="submit" value="Enviar">
+        <p id="resultado"></p>
+    </form>
+    
+
+        <?php
+             if (isset($_GET["numero"])) {
+                
+            }
+                
+          
+
+        ?>
+    <script>
+        
+    </script>
+</body>
+</html>
--- php/php(3)/triangulo.php
+++ php/php(3)/triangulo.php
@@ -14,7 +14,7 @@
 		$numero = htmlspecialchars($_GET["numero"]);        // evita inyecciones de código
 		if ($numero>0 && is_numeric($numero)){              //comprueba que es positivo y es numérico
 			
-    /*    if ($numero>0 && is_numeric($numero)){              //comprueba que es positivo y es numérico
+    /*   if ($numero>0 && is_numeric($numero)){    */          //comprueba que es positivo y es numérico
             for ($i=1;$i<=$numero;$i++)     // inicia la variable con el valor de numero ingresado por usuario // El bucle continuará ejecutándose mientras $i sea mayor o igual a 1 Después de cada iteración, $i se decrementa >
             {       
                     for($j=1;$j<=$i;$j++)       // esto es para hacerlo de menor a mayor //el otro para hacerlo de mayor a menor
